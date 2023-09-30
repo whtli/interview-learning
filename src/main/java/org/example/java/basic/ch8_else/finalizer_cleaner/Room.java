5ae99a990c254837eb9011677585c9cc4bbc39a4
whtli <3239349119@qq.com>
2023-09-30 11:16:37 +0800

    避免使用finalizer()和cleaner()

---
+++ src/main/java/org/example/java/basic/ch8_else/finalizer_cleaner/Room.java
@@ -0,0 +1,60 @@
+package org.example.java.basic.ch8_else.finalizer_cleaner;
+
+
+import java.lang.ref.Cleaner;
+
+/**
+ * @author: whtli
+ * @date: 2023/09/28
+ * @description: 避免使用终结方法和清楚方法
+ * @source: Effective Java
+ * An autocloseable class using a cleaner as a safety net
+ * 需要配置jdk9及以上才可以允许，8没有相关依赖
+ */
+public class Room implements AutoCloseable {
+    private static final Cleaner cleaner = Cleaner.create();
+
+    // Resource that requires cleaning. Must not refer to Room
+    private static class State implements Runnable {
+        // Number of junk piles in this room
+        int numJunkPiles;
+
+        State(int numJunkPiles) {
+            this.numJunkPiles = numJunkPiles;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Cleaning room");
+            numJunkPiles = 0;
+        }
+    }
+
+    // The state of this room, shared with our cleanable
+    private final State state;
+
+    // Our cleanable. Cleans the room when it's eligible for gc
+    private final Cleaner.Cleanable cleanable;
+
+    public Room(int numJunkPoles) {
+        state = new State(numJunkPoles);
+        cleanable = cleaner.register(this, state);
+    }
+
+    @Override
+    public void close() throws Exception {
+        cleanable.clean();
+    }
+}
+
+class Adult {
+    public static void main(String[] args) throws Exception {
+        try (Room myRoom = new Room(7)) {
+            System.out.println("goodbye");
+        }
+
+        System.out.println();
+        new Room(99);
+        System.out.println("Peace out");
+    }
+}
