61ea35ecc3ae5a764f4a9832bda8b64fc0b74828
whtli <3239349119@qq.com>
2023-10-07 16:48:36 +0800

    ThreadLocal相关

--- src/main/java/org/example/ch01_java/ch04_concurrent/thread/ThreadLocalExample.java
+++ src/main/java/org/example/ch01_java/ch04_concurrent/thread/ThreadLocalExample.java
@@ -6,20 +6,10 @@ import java.util.Random;
 /**
  * @author: whtli
  * @date: 2023/03/05
- * @description:
+ * @description: ThreadLocal
  */
 public class ThreadLocalExample implements Runnable {
-    public static final ThreadLocal<SimpleDateFormat> formatter = ThreadLocal.withInitial(() ->
-            new SimpleDateFormat("yyyyMMdd HHmm"));
-
-    public static void main(String[] args) throws InterruptedException {
-        ThreadLocalExample obj = new ThreadLocalExample();
-        for (int i = 0; i < 10; i++) {
-            Thread t = new Thread(obj, "" + i);
-            Thread.sleep(new Random().nextInt(1000));
-            t.start();
-        }
-    }
+    private static final ThreadLocal<SimpleDateFormat> formatter = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MMdd-HHmm"));
 
     @Override
     public void run() {
@@ -29,7 +19,22 @@ public class ThreadLocalExample implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        // formatter的样式在此处被当前线程更改，但这个变更不会影响到其他线程
         formatter.set(new SimpleDateFormat());
         System.out.println("Thread Name : " + Thread.currentThread().getName() + "; Default Formatter : " + formatter.get().toPattern());
+    }
+
+    public static void main(String[] args) {
+        ThreadLocalExample obj = new ThreadLocalExample();
+        // 观察输出结果可以得知，虽然Thread-i已经改变了formatter的值，但Thread-j默认格式化值与初始化值相同
+        for (int i = 0; i < 5; i++) {
+            Thread t = new Thread(obj, "" + i);
+            try {
+                Thread.sleep(new Random().nextInt(1000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            t.start();
+        }
     }
 }
