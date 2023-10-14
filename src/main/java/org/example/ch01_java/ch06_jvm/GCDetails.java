bf2858bc2648236ec87546a850e7e51694499280
whtli <3239349119@qq.com>
2023-10-14 11:21:34 +0800

    GC相关

--- src/main/java/org/example/ch01_java/ch06_jvm/GCDetails.java
+++ src/main/java/org/example/ch01_java/ch06_jvm/GCDetails.java
@@ -4,10 +4,15 @@ package org.example.ch01_java.ch06_jvm;
  * @author: whtli
  * @date: 2023/07/15
  * @description: JVM垃圾回收详解
+ * 需要添加VM options：-XX:+PrintGCDetails
  */
 public class GCDetails {
     public static void main(String[] args) {
-        byte[] allocation1, allocation2;
-        allocation1 = new byte[2000000000*2000];
+        byte[] allocation1, allocation2, allocation3, allocation4, allocation5;
+        allocation1 = new byte[32000 * 1024];
+        allocation2 = new byte[1000 * 1024];
+        allocation3 = new byte[1000 * 1024];
+        allocation4 = new byte[1000 * 1024];
+        allocation5 = new byte[1000 * 1024];
     }
 }
--- src/main/java/org/example/ch01_java/ch06_jvm/PrintClassLoaderTree.java
+++ src/main/java/org/example/ch01_java/ch06_jvm/PrintClassLoaderTree.java
@@ -9,19 +9,23 @@ public class PrintClassLoaderTree {
 
     public static void main(String[] args) {
 
+        // Spring获取线程线程上下文类加载器
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println(contextClassLoader);
+        System.out.println();
+
         ClassLoader classLoader = PrintClassLoaderTree.class.getClassLoader();
 
         StringBuilder split = new StringBuilder("|--");
         boolean needContinue = true;
-        while (needContinue){
+        while (needContinue) {
             System.out.println(split.toString() + classLoader);
-            if(classLoader == null){
+            if (classLoader == null) {
                 needContinue = false;
-            }else{
+            } else {
                 classLoader = classLoader.getParent();
                 split.insert(0, "\t");
             }
         }
     }
-
 }
