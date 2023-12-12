39f6a2de1e0637e179a5a457fa74d26c36781902
whtli <3239349119@qq.com>
2023-12-12 22:25:13 +0800

    用接口模拟可扩展的枚举

---
+++ src/main/java/org/example/ch01_java/ch01_basic/p17_enum/simulate_extensible_enum_with_interfaces/BasicOperation.java
@@ -0,0 +1,40 @@
+package org.example.ch01_java.ch01_basic.p17_enum.simulate_extensible_enum_with_interfaces;
+
+/**
+ * @author: whtli
+ * @date: 2023/12/12
+ * @description: 用接口模拟可扩展的枚举
+ */
+public enum BasicOperation implements Operation {
+    PLUS("+") {
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES("*") {
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    private final String symbol;
+
+    BasicOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+}
